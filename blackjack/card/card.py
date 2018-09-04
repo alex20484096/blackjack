740173b88c14d60fd72f8a04d03024b41a904814
Conor <conor.mullen@ft.com>
2018-09-04 12:58:17 +0100

    Now with comments

--- blackjack/card/card.py
+++ blackjack/card/card.py
@@ -1,10 +1,11 @@
-
-
 class Card(object):
     SUITS = ['Club', 'Diamond', 'Heart', 'Spade']
     VALUES = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
 
     def __init__(self, suit, value):
+        '''
+        Check if supplied parameters are valid before setting attributes
+        '''
         if suit in self.SUITS and value in self.VALUES:
             self.suit = suit
             self.value = value
@@ -12,10 +13,25 @@ class Card(object):
             raise ValueError
 
     def __str__(self):
+        '''
+        Nice string version of a card e.g. A of Spades
+        Used when anything tries to conver the Card to a string e.g. print()
+        '''
         return str(f'{self.value} of {self.suit}s')
 
     def __repr__(self):
+        '''
+        Uses __str__
+        Used to represent the Card
+        '''
         return self.__str__()
 
     def __eq__(self, other):
-        return self.suit == other.suit and self.value == other.value
+        '''
+        Used when something trys to compare equality of a card
+        One card is equal to another if they match suit and value
+        '''
+        try:
+            return self.suit == other.suit and self.value == other.value
+        except AttributeError:  # Will be raised if other is not a Card
+            return False
--- blackjack/deck/deck.py
+++ blackjack/deck/deck.py
@@ -3,15 +3,28 @@ from card import Card
 
 
 class Deck(list):
+    '''
+    The deck is essentially a list of cards so inherits from list
+    '''
+
     def __init__(self):
+        '''
+        Initialise the parent list and reset the deck
+        '''
         list.__init__(self)
         self.reset()
 
     def reset(self):
+        '''
+        Empty the deck and then populate with all of the possible cards
+        '''
         self.clear()
         for suit in Card.SUITS:
             for value in Card.VALUES:
                 self.append(Card(suit, value))
 
     def shuffle(self):
+        '''
+        Use the `shuffle` function from the `random` module to shuffle the deck
+        '''
         random.shuffle(self)
